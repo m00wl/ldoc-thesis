93e8b2f5f53c5a3a4aec759275e474043638b9e8
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2023-01-25 14:54:14 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "m00wl large document thesis";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-small latexmk tudscr
+            newunicodechar;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ tex ];
+          shellHook = ''
+            export LDOC_THESIS_ROOT=$(git rev-parse --show-toplevel)
+            alias mt="env SOURCE_DATE_EPOCH=$(date +%s) latexmk -pdf -lualatex -auxdir=$LDOC_THESIS_ROOT/thesis/build $LDOC_THESIS_ROOT/thesis/ldoc-thesis.tex"
+          '';
+        };
+      };
+      flake = {};
+    };
+
+  nixConfig = {
+    bash-prompt-suffix = "dev: ";
+  };
+}
